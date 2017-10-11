277c84abf165be2f5e1098f5a32cd5c9b80e7ed1
JFer <jfer@jferUb>
2017-10-11 14:49:42 -0500

    segunda

---
+++ Primero/src/alcance/variables/alcance.kt
@@ -0,0 +1,11 @@
+package alcance.variables
+//the Unit object. This type corresponds to the void type in Java.
+
+var otroNombre = "Global"
+fun display():Unit{
+	
+}
+
+fun main(args: Array<String>) {
+	var nombre = "Kotlin"
+}
